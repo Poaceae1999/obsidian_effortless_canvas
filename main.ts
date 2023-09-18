c4691f226b090d1bb1bf9496f53c34a24a724a3d
Poaceae1999 <b07601009@g.ntu.edu.tw>
2023-09-18 11:11:29 +0800

    one of feature is done : open focused file .

--- main.ts
+++ main.ts
@@ -1,134 +1,102 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, FileView, Modal, Notice, Plugin, PluginSettingTab, Setting, ItemView, WorkspaceLeaf } from 'obsidian';
+	const fileName = 'ewr/tet.md';
+const data = 'tet';
+var import_obsidian = require("obsidian");
 
-// Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
-	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+export default class effortless_canvas extends Plugin {
+	getActiveCanvas() {
+		var _a;
+		const maybeCanvasView = (_a = this.app.workspace.activeLeaf) == null ? void 0 : _a.view;
+		return maybeCanvasView ? maybeCanvasView["canvas"] : null;
+	};
+	getFocusedFileData() {
+		return [...this.getActiveCanvas().selection]
+	};
+	openFocusedNode() {
+		this.app.workspace.openLinkText(
+			this.getFocusedFileData()[0].file.path,
+			"",
+			"tab"
+		)
 	}
+	printFocusedCanvasData_dev() {
+		console.log(this.getActiveCanvas().selectionChanged)
+	};
+	
+	focusOnNode(canvas, node) {
+		canvas.zoomToBbox({
+			minX: node.x - node.width * 1,
+			minY: node.y - node.height * 1,
+			maxX: node.x + node.width * 1,
+			maxY: node.y + node.height * 1
+		})
+	};
 
+	// 創建一個 Proxy，監聽 filePath 屬性的變化
+	handler = {
+		set(target, property, value) {
+			if (property === 'path' && value !== target[property]) {
+				// 在 filePath 值改變時執行你的函數
+				console.log('yes');
+				this.openFocusedNode();
+				return true;
+			} else {
+				return false;
+			}
+		},
+	};
+
+	proxyObject = new Proxy(this.getFocusedFileData()[0].file, this.handler);
+
+	FocusSpecificFile(data) {
+		const canvasView = this.app.workspace.getActiveViewOfType(import_obsidian.ItemView);
+		this.registerEvent(import_obsidian.HTMLElement.onClickevent())
+		const canvas = "";
+	if ((canvasView == null ? void 0 : canvasView.getViewType()) === "canvas") {
+		const canvas2 = canvasView.canvas;
+	};
+	let node_data = data[0];
+	switch (node_data.type) {
+		case "file":
+			new import_obsidian.Notice(`Selected ${node_data.file}`);
+			this.focusOnNode(this.getActiveCanvas(), node_data);
+			break;
+		case "group":
+			new import_obsidian.Notice(`Selected ${node_data.file}`);
+			this.focusOnNode(this.getActiveCanvas(), node_data);
+			break;
+		case "text":
+			new import_obsidian.Notice(`Selected ${node_data.text}`);
+			this.focusOnNode(this.getActiveCanvas(), node_data);
+			break;
+		case "link":
+			new import_obsidian.Notice(`Selected ${node_data.url}`);
+			break;
+		};
+}
+	async onload() {
+		this.addCommand({
+			id: `openFocusedNode`,
+			name: `openFocusedNode`,
+			callback: () => {
+				this.openFocusedNode()
+				;
+			}
+		});
+		this.addCommand({
+			id: `dev_used`,
+			name: `dev_used`,
+			callback: () => {
+				this.printFocusedCanvasData_dev()
+				;
+			}
+		});
+		
+	}
 	onunload() {
 
 	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+	
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
-}
