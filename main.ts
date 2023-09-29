b7d966510dfcdd8ffedd4bdc162d5877dd16d4df
Poaceae1999 <b07601009@g.ntu.edu.tw>
2023-09-29 17:18:37 +0800

    error handling when getFocusedFileData() do not get filename.

--- main.ts
+++ main.ts
@@ -1,102 +1,169 @@
-import { App, Editor, FileView, Modal, Notice, Plugin, PluginSettingTab, Setting, ItemView, WorkspaceLeaf } from 'obsidian';
-	const fileName = 'ewr/tet.md';
-const data = 'tet';
+import { App, Editor, FileView, Modal, Notice, Plugin, PluginSettingTab, Setting, ItemView,MarkdownView, WorkspaceLeaf, WorkspaceRoot, MarkdownEditView } from 'obsidian';
+import { isNull } from 'util';
 var import_obsidian = require("obsidian");
+import { ExampleView, VIEW_TYPE_EXAMPLE ,MarkdownSourceView} from "./view";
+export const VIEW_TYPE_EXAMPLE = "example-view";
 
+interface effortless_canvas_setting {
+	PluginIsTurnedOn:boolean
+}
 
+const DEFAULT_SETTINGS: effortless_canvas_setting = {
+	PluginIsTurnedOn:true
+}
+
+export class ExampleView extends MarkdownView {
+	constructor(leaf: WorkspaceLeaf) {
+		super(leaf);
+	}
+
+	getViewType() {
+		return VIEW_TYPE_EXAMPLE;
+	}
+
+	getDisplayText() {
+		return "Example view";
+	}
+
+	async onOpen() {
+		const container = this.containerEl.children[1];
+
+	}
+
+	async onClose() {
+		// Nothing to clean up.
+	}
+}
 export default class effortless_canvas extends Plugin {
+	settings: effortless_canvas_setting
+	tabLeaf: WorkspaceLeaf | null = null;
+	PluginIsTurnedOn = true;
 	getActiveCanvas() {
 		var _a;
 		const maybeCanvasView = (_a = this.app.workspace.activeLeaf) == null ? void 0 : _a.view;
 		return maybeCanvasView ? maybeCanvasView["canvas"] : null;
 	};
 	getFocusedFileData() {
-		return [...this.getActiveCanvas().selection]
+		try {
+			return [...this.getActiveCanvas().selection][0].file.path
+		}
+		catch (error) {
+			console.log('no file')
+			return(false)
+		}
 	};
+
+
 	openFocusedNode() {
-		this.app.workspace.openLinkText(
-			this.getFocusedFileData()[0].file.path,
-			"",
-			"tab"
-		)
-	}
+		if (this.getFocusedFileData() != false) {
+			const focus_note = this.getFocusedFileData()
+			this.app.workspace.openLinkText(
+				focus_note,
+				'',
+				'tab'
+			);
+		}
+	};
 	printFocusedCanvasData_dev() {
-		console.log(this.getActiveCanvas().selectionChanged)
-	};
-	
-	focusOnNode(canvas, node) {
-		canvas.zoomToBbox({
-			minX: node.x - node.width * 1,
-			minY: node.y - node.height * 1,
-			maxX: node.x + node.width * 1,
-			maxY: node.y + node.height * 1
-		})
+		console.log(this.getFocusedFileData())
+		console.log(this.app.workspace.activeLeaf.parent.children[1].id)
 	};
 
-	// 創建一個 Proxy，監聽 filePath 屬性的變化
-	handler = {
-		set(target, property, value) {
-			if (property === 'path' && value !== target[property]) {
-				// 在 filePath 值改變時執行你的函數
-				console.log('yes');
-				this.openFocusedNode();
-				return true;
-			} else {
-				return false;
-			}
-		},
+	ModeSwitcher(PluginIsTurnedOn: boolean) {
+		// if Plugin Is Turned On return false
+		// if Plugin Isn't Turned On return true
+		return (this.settings.PluginIsTurnedOn == false)
 	};
 
-	proxyObject = new Proxy(this.getFocusedFileData()[0].file, this.handler);
 
-	FocusSpecificFile(data) {
-		const canvasView = this.app.workspace.getActiveViewOfType(import_obsidian.ItemView);
-		this.registerEvent(import_obsidian.HTMLElement.onClickevent())
-		const canvas = "";
-	if ((canvasView == null ? void 0 : canvasView.getViewType()) === "canvas") {
-		const canvas2 = canvasView.canvas;
-	};
-	let node_data = data[0];
-	switch (node_data.type) {
-		case "file":
-			new import_obsidian.Notice(`Selected ${node_data.file}`);
-			this.focusOnNode(this.getActiveCanvas(), node_data);
-			break;
-		case "group":
-			new import_obsidian.Notice(`Selected ${node_data.file}`);
-			this.focusOnNode(this.getActiveCanvas(), node_data);
-			break;
-		case "text":
-			new import_obsidian.Notice(`Selected ${node_data.text}`);
-			this.focusOnNode(this.getActiveCanvas(), node_data);
-			break;
-		case "link":
-			new import_obsidian.Notice(`Selected ${node_data.url}`);
-			break;
-		};
-}
+	async loadSettings() {
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData()
+		);
+	}
+
+	async saveSettings() {
+		await this.saveData(this.settings);
+	}
+
+
 	async onload() {
+		await this.loadSettings();
+
+		this.registerView(
+			VIEW_TYPE_EXAMPLE,
+			(leaf) => new ExampleView(leaf)
+		);
+		
+		//this.addRibbonIcon("dice", "Activate view", () => {
+		//	this.activateView();
+		//});
+
+		this.addRibbonIcon("switch", "Turn on/off effortless canvas", () => {
+			this.settings.PluginIsTurnedOn = this.ModeSwitcher(this.settings.PluginIsTurnedOn);
+		});
+
 		this.addCommand({
 			id: `openFocusedNode`,
 			name: `openFocusedNode`,
 			callback: () => {
 				this.openFocusedNode()
-				;
+					;
 			}
 		});
 		this.addCommand({
 			id: `dev_used`,
 			name: `dev_used`,
 			callback: () => {
-				this.printFocusedCanvasData_dev()
-				;
+				this.app.workspace.openLinkText(
+					this.getFocusedFileData(),
+					'',
+					'tab'
+				);
 			}
 		});
-		
+
+		this.registerEvent(this.app.workspace.on('file-open', (file) => {
+			// 在這裡執行你的指令，file 是被選擇的文件對象
+			
+			if (this.getFocusedFileData() === false) {
+				if (this.getActiveCanvas() == undefined) {
+				// focusing on markdown / non-canvas type do nothing(for view/editing)
+					return (0)
+				}
+				else {
+				// focusing on canvas type then close all of md file
+					this.app.workspace.detachLeavesOfType('markdown');
+					return(0)
+				}
+			}	
+			if (this.settings.PluginIsTurnedOn == true  ) { 
+				this.app.workspace.detachLeavesOfType('markdown');
+				
+				this.openFocusedNode();
+				this.printFocusedCanvasData_dev();
+
+			}
+		}));
 	}
+
 	onunload() {
 
 	}
-	
-}
 
+	async activateView() {
+		this.app.workspace.detachLeavesOfType(VIEW_TYPE_EXAMPLE);
+
+		await this.app.workspace.getRightLeaf(false).setViewState({
+			type: VIEW_TYPE_EXAMPLE,
+			active: true,
+		});
+
+		this.app.workspace.revealLeaf(
+			this.app.workspace.getLeavesOfType(VIEW_TYPE_EXAMPLE)[0]
+		);
+	}
+
+}
